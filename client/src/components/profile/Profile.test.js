3cf06aa9837a7f8201a3ea849b7ec789057b1e36
Mickey <vickeylee.w@gmail.com>
2020-08-05 17:04:02 +1000

    Add Profile/ProfileTop/ProfileAbout component testing

--- client/src/components/profile/Profile.test.js
+++ client/src/components/profile/Profile.test.js
@@ -1,224 +1,339 @@
-// import React from 'react';
-// import { shallow, mount } from 'enzyme';
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-// import { findByTestAttr, storeFactory } from '../../../test/testUtils';
-// import Profile from './Profile';
+import { findByTestAttr, storeFactory } from '../../../test/testUtils';
+import Profile, { UnconnectedProfile } from './Profile';
 
-// //Factory function to create a ShallowWrapper for the Profile component.
-// const setup = (initialState = {}) => {
-//   const store = storeFactory(initialState);
-//   const wrapper = shallow(<Profile store={store} />).dive();
-//   return wrapper;
-// };
+//Factory function to create a ShallowWrapper for the Profile component.
+const setup = (initialState = {}, props) => {
+  const store = storeFactory(initialState);
+  const wrapper = mount(
+    <Router>
+      <Profile store={store} {...props} />
+    </Router>
+  );
+  return wrapper;
+};
 
-// describe('render', () => {
-//   describe('user is authenticated', () => {
-//     let wrapper;
-//     const historyMock = { push: jest.fn() };
-//     const paramsMock = { id: jest.fn() };
-//     beforeEach(() => {
-//       const initialState = {
-//         auth: {
-//           token: 'token',
-//           isAuthenticated: true,
-//           loading: true,
-//           user: null
-//         },
-//         profile: {
-//           profile: {},
-//           profiles: [],
-//           repos: [],
-//           loading: true,
-//           error: {}
-//         }
-//       };
-//       wrapper = setup(initialState);
-//     });
-//     test('renders component without error', () => {
-//       const component = findByTestAttr(wrapper, 'component-profile');
-//       expect(component.length).toBe(1);
-//     });
-//     test('renders profile top', () => {
-//       const profileTop = findByTestAttr(wrapper, 'profile-top');
-//       expect(profileTop.length).toBe(1);
-//     });
-//     test('renders profile about', () => {
-//       const profileAbout = findByTestAttr(wrapper, 'profile-about');
-//       expect(profileAbout.length).toBe(1);
-//     });
-//     test('renders profile Github', () => {
-//       const profileGithub = findByTestAttr(wrapper, 'profile-github');
-//       expect(profileGithub.length).toBe(1);
-//     });
-//     test('renders edit profile', () => {
-//       const editProfile = findByTestAttr(wrapper, 'edit-profile');
-//       expect(editProfile.length).toBe(1);
-//     });
-//   });
-//   describe('user is not authenticated', () => {
-//     let wrapper;
-//     const historyMock = { push: jest.fn() };
-//     const paramsMock = { id: jest.fn() };
-//     beforeEach(() => {
-//       const initialState = {
-//         auth: {
-//           token: '',
-//           isAuthenticated: false,
-//           loading: true,
-//           user: null
-//         },
-//         profile: {
-//           profile: {},
-//           profiles: [],
-//           repos: [],
-//           loading: true,
-//           error: {}
-//         }
-//       };
-//       wrapper = setup(initialState);
-//     });
-//     test('renders component without error', () => {
-//       const component = findByTestAttr(wrapper, 'component-profile');
-//       expect(component.length).toBe(1);
-//     });
-//     test('renders profile top', () => {
-//       const profileTop = findByTestAttr(wrapper, 'profile-top');
-//       expect(profileTop.length).toBe(1);
-//     });
-//     test('renders profile about', () => {
-//       const profileAbout = findByTestAttr(wrapper, 'profile-about');
-//       expect(profileAbout.length).toBe(1);
-//     });
-//     test('renders profile Github', () => {
-//       const profileGithub = findByTestAttr(wrapper, 'profile-github');
-//       expect(profileGithub.length).toBe(1);
-//     });
-//     test('renders edit profile', () => {
-//       const editProfile = findByTestAttr(wrapper, 'edit-profile');
-//       expect(editProfile.length).toBe(0);
-//     });
-//   });
-// });
+describe('render', () => {
+  describe('user is authenticated', () => {
+    let wrapper;
 
-// describe('redux props', () => {
-//   test('has auth and profile state as prop', () => {
-//     const initialState = {
-//       auth: {
-//         token: '',
-//         isAuthenticated: false,
-//         loading: true,
-//         user: null
-//       },
-//       profile: {
-//         profile: {},
-//         profiles: [],
-//         repos: [],
-//         loading: true,
-//         error: {}
-//       }
-//     };
-//     const wrapper = setup(initialState);
-//     const authProp = wrapper.instance().props.auth;
-//     const profileProp = wrapper.instance().props.profile;
-//     expect(authProp).toEqual(initialState.auth);
-//     expect(profileProp).toEqual(initialState.profile);
-//   });
-//   test('`getProfileById` action creator is a function prop', () => {
-//     const wrapper = setup();
-//     const getProfileByIdProp = wrapper.instance().props.getProfileById;
-//     expect(getProfileByIdProp).toBeInstanceOf(Function);
-//   });
-//   test('`deleteEmployee` action creator is a function prop', () => {
-//     const wrapper = setup();
-//     const deleteEmployeeProp = wrapper.instance().props.deleteEmployee;
-//     expect(deleteEmployeeProp).toBeInstanceOf(Function);
-//   });
-// });
+    beforeEach(() => {
+      const initialState = {
+        profile: {
+          profile: {
+            bio: 'hi',
+            skills: ['JS', 'Python'],
+            githubusername: null,
+            status: 'intern',
+            company: 'A&B',
+            location: 'AU',
+            user: { name: 'Amy', avatar: 'link' }
+          },
+          loading: false
+        },
+        auth: {
+          isAuthenticated: true,
+          loading: false,
+          user: {
+            isAdmin: true
+          }
+        }
+      };
+      const props = {
+        match: { params: { id: 'id' } },
+        history: { push: jest.fn() }
+      };
 
-// describe('useEffect', () => {
-//   const initialState = {
-//     auth: {
-//       token: '',
-//       isAuthenticated: false,
-//       loading: true,
-//       user: null
-//     },
-//     profile: {
-//       profile: {},
-//       profiles: [],
-//       repos: [],
-//       loading: true,
-//       error: {}
-//     }
-//   };
-//   const historyMock = { push: jest.fn() };
-//   const paramsMock = { id: jest.fn() };
-//   beforeEach(() => {
-//     const store = storeFactory(initialState);
-//     const wrapper = mount(<Profile store={store} />);
+      wrapper = setup(initialState, props);
+    });
+    test('renders profile top', () => {
+      const profileTop = findByTestAttr(wrapper, 'component-profile-top');
+      expect(profileTop.length).toBe(1);
+    });
+    test('renders profile about', () => {
+      const profileAbout = findByTestAttr(wrapper, 'component-profile-about');
+      expect(profileAbout.length).toBe(1);
+    });
+    test('not renders profile Github with githubusername null', () => {
+      const profileGithub = findByTestAttr(wrapper, 'component-profile-github');
+      expect(profileGithub.length).toBe(0);
+    });
+    test('renders edit profile with isAuthenticated true', () => {
+      const editProfile = findByTestAttr(wrapper, 'edit-profile');
+      expect(editProfile.length).toBeGreaterThan(0);
+    });
+    test('not renders delete-button with isAdmin false', () => {
+      wrapper = setup(
+        {
+          profile: {
+            profile: {
+              bio: 'hi',
+              skills: ['JS', 'Python'],
+              githubusername: null,
+              status: 'intern',
+              company: 'A&B',
+              location: 'AU',
+              user: { name: 'Amy', avatar: 'link' }
+            },
+            loading: false
+          },
+          auth: {
+            isAuthenticated: true,
+            user: {
+              isAdmin: false
+            }
+          }
+        },
+        {
+          match: { params: { id: 'id' } },
+          history: { push: jest.fn() }
+        }
+      );
+      const editProfile = findByTestAttr(wrapper, 'delete-button');
+      expect(editProfile.length).toBe(0);
+    });
+    test('renders spinner with profile === null', () => {
+      wrapper = setup(
+        {
+          profile: {
+            profile: null,
+            loading: false
+          },
+          auth: {
+            isAuthenticated: true,
+            user: {
+              isAdmin: true
+            }
+          }
+        },
+        {
+          match: { params: { id: 'id' } },
+          history: { push: jest.fn() }
+        }
+      );
+      const editProfile = findByTestAttr(wrapper, 'component-spinner');
+      expect(editProfile.length).toBe(1);
+    });
+    test('renders spinner with loading true', () => {
+      wrapper = setup(
+        {
+          profile: {
+            profile: {
+              bio: 'hi',
+              skills: ['JS', 'Python'],
+              githubusername: null,
+              status: 'intern',
+              company: 'A&B',
+              location: 'AU',
+              user: { name: 'Amy', avatar: 'link' }
+            },
+            loading: true
+          },
+          auth: {
+            isAuthenticated: true,
+            user: {
+              isAdmin: true
+            }
+          }
+        },
+        {
+          match: { params: { id: 'id' } },
+          history: { push: jest.fn() }
+        }
+      );
+      const editProfile = findByTestAttr(wrapper, 'component-spinner');
+      expect(editProfile.length).toBe(1);
+    });
+  });
 
-//     const mockGetProfileById = jest.fn();
-//     mockGetProfileById.mockClear();
-//     wrapper.instance().props.getProfileById = mockGetProfileById;
-//   });
+  describe('user is not authenticated', () => {
+    let wrapper;
 
-//   test('getProfileById gets called on Profile mount', () => {
-//     expect(mockGetProfileById).toHaveBeenCalled();
-//   });
-//   test('`getProfileById` does not update when Profile update', () => {
-//     mockGetSecretWord.mockClear();
+    beforeEach(() => {
+      const initialState = {
+        profile: {
+          profile: {
+            bio: 'hi',
+            skills: ['JS', 'Python'],
+            githubusername: null,
+            status: 'intern',
+            company: 'A&B',
+            location: 'AU',
+            user: { name: 'Amy', avatar: 'link' }
+          },
+          loading: false
+        },
+        auth: {
+          isAuthenticated: false,
+          loading: false,
+          user: {
+            isAdmin: false
+          }
+        }
+      };
+      const props = {
+        match: { params: { id: 'id' } },
+        history: { push: jest.fn() }
+      };
 
-//     wrapper.setProps();
+      wrapper = setup(initialState, props);
+    });
+    test('renders profile top', () => {
+      const profileTop = findByTestAttr(wrapper, 'component-profile-top');
+      expect(profileTop.length).toBe(1);
+    });
+    test('renders profile about', () => {
+      const profileAbout = findByTestAttr(wrapper, 'component-profile-about');
+      expect(profileAbout.length).toBe(1);
+    });
+    test('not renders profile Github with githubusername null', () => {
+      const profileGithub = findByTestAttr(wrapper, 'component-profile-github');
+      expect(profileGithub.length).toBe(0);
+    });
+    test('not renders edit profile', () => {
+      const editProfile = findByTestAttr(wrapper, 'edit-profile');
+      expect(editProfile.length).toBe(0);
+    });
+    test('not renders delete-button with isAdmin false', () => {
+      const deleteButton = findByTestAttr(wrapper, 'delete-button');
+      expect(deleteButton.length).toBe(0);
+    });
 
-//     expect(mockGetSecretWord).not.toHaveBeenCalled();
-//   });
-// });
+    test('renders spinner with profile === null', () => {
+      wrapper = setup(
+        {
+          profile: {
+            profile: null,
+            loading: false
+          },
+          auth: {
+            isAuthenticated: false,
+            user: {
+              isAdmin: false
+            }
+          }
+        },
+        {
+          match: { params: { id: 'id' } },
+          history: { push: jest.fn() }
+        }
+      );
+      const editProfile = findByTestAttr(wrapper, 'component-spinner');
+      expect(editProfile.length).toBe(1);
+    });
+    test('renders spinner with loading true', () => {
+      wrapper = setup(
+        {
+          profile: {
+            profile: {
+              bio: 'hi',
+              skills: ['JS', 'Python'],
+              githubusername: null,
+              status: 'intern',
+              company: 'A&B',
+              location: 'AU',
+              user: { name: 'Amy', avatar: 'link' }
+            },
+            loading: true
+          },
+          auth: {
+            isAuthenticated: false,
+            user: {
+              isAdmin: false
+            }
+          }
+        },
+        {
+          match: { params: { id: 'id' } },
+          history: { push: jest.fn() }
+        }
+      );
+      const editProfile = findByTestAttr(wrapper, 'component-spinner');
+      expect(editProfile.length).toBe(1);
+    });
+  });
+});
 
-// // // Challenge #3: Give Up Button
-// // test('calls `giveUp` prop upon "Give Up" button click', () => {
-// //   // create a mock function so we can see whether it's called on click
-// //   const giveUpMock = jest.fn();
+test('calls `deleteEmployee` prop upon "deleteEmployee" button click', () => {
+  const deleteEmployeeMock = jest.fn();
+  const getProfileByIdMock = jest.fn();
+  const props = {
+    profile: {
+      profile: {
+        bio: 'hi',
+        skills: ['JS', 'Python'],
+        githubusername: null,
+        status: 'intern',
+        company: 'A&B',
+        location: 'AU',
+        user: { name: 'Amy', avatar: 'link' }
+      },
+      loading: false
+    },
+    auth: {
+      user: { isAdmin: true }
+    },
+    match: { params: { id: 'id' } },
+    history: { push: jest.fn() }
+  };
+  const wrapper = shallow(
+    <UnconnectedProfile
+      {...props}
+      deleteEmployee={deleteEmployeeMock}
+      getProfileById={getProfileByIdMock}
+    />
+  );
+  // simulate click on button
+  const deleteButton = findByTestAttr(wrapper, 'delete-button');
+  deleteButton.simulate('click');
 
-// //   // set up Input, with giveUpMock as a prop
-// //   const wrapper = shallow(<UnconnectedProfile giveUp={giveUpMock} />);
+  // expect the mock to have been called once
+  expect(deleteEmployeeMock.mock.calls.length).toBe(1);
+});
 
-// //   // simulate click on giveUp button
-// //   const giveUpButton = findByTestAttr(wrapper, 'give-up-button');
-// //   giveUpButton.simulate('click', { preventDefault() {} });
+describe('redux props', () => {
+  let wrapper;
+  const initialState = {
+    profile: {
+      profile: {
+        bio: 'hi',
+        skills: ['JS', 'Python'],
+        githubusername: null,
+        status: 'intern',
+        company: 'A&B',
+        location: 'AU',
+        user: { name: 'Amy', avatar: 'link' }
+      },
+      loading: false
+    },
+    auth: {
+      isAuthenticated: false,
+      loading: false,
+      user: {
+        isAdmin: false
+      }
+    }
+  };
+  beforeEach(() => {
+    const store = storeFactory(initialState);
+    wrapper = shallow(<Profile store={store} />).dive();
+  });
 
-// //   // expect the mock to have been called once
-// //   expect(giveUpMock.mock.calls.length).toBe(1);
-// // });
-// // // END: Challenge #3: Give Up Button
-
-// // describe('`guessWord` action creator', () => {
-// //   let guessWordMock;
-// //   let wrapper;
-// //   const guessedWord = 'train';
-// //   beforeEach(() => {
-// //     // create a mock function for `guessWord`
-// //     guessWordMock = jest.fn();
-
-// //     // set up Input, with guessWordMock as a prop
-// //     wrapper = shallow(<UnconnectedProfile guessWord={guessWordMock} />);
-
-// //     // simulate the input
-// //     wrapper.instance().inputBox.current = { value: guessedWord };
-
-// //     // simulate click on submit button
-// //     const submit = findByTestAttr(wrapper, 'submit-button');
-// //     submit.simulate('click', { preventDefault() {} });
-// //   });
-// //   test('`guessWord` was called once', () => {
-// //     const guessWordCallCount = guessWordMock.mock.calls.length;
-// //     expect(guessWordCallCount).toBe(1);
-// //   });
-// //   test('`guessWord` was called with input value as argument', () => {
-// //     const guessedWordArg = guessWordMock.mock.calls[0][0];
-// //     expect(guessedWordArg).toBe(guessedWord);
-// //   });
-// //   test('input box clears on submit', () => {
-// //     expect(wrapper.instance().inputBox.current.value).toBe('');
-// //   })
-
-// // });
+  test('has auth and profile state as prop', () => {
+    const authProp = wrapper.props().auth;
+    const profileProp = wrapper.props().profile;
+    expect(authProp).toEqual(initialState.auth);
+    expect(profileProp).toEqual(initialState.profile);
+  });
+  test('`getProfileById` action creator is a function prop', () => {
+    const getProfileByIdProp = wrapper.props().getProfileById;
+    expect(getProfileByIdProp).toBeInstanceOf(Function);
+  });
+  test('`deleteEmployee` action creator is a function prop', () => {
+    const deleteEmployeeProp = wrapper.props().deleteEmployee;
+    expect(deleteEmployeeProp).toBeInstanceOf(Function);
+  });
+});
