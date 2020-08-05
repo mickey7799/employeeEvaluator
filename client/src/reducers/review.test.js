7a464210a33de590d42b4696bd041af058d01e40
Mickey <vickeylee.w@gmail.com>
2020-08-05 20:53:48 +1000

    Update reducer review testing

--- client/src/reducers/review.test.js
+++ client/src/reducers/review.test.js
@@ -16,6 +16,7 @@ describe('Review reducer testing', () => {
   const defaultState = { reviews: [], review: null, loading: true, error: {} };
   const review = {
     user: '5f24ee06a2c4e08896cd1b3c',
+    _id: 1,
     admin: '5f229ece17d7768ccff85190',
     text: 'R&D',
     bio: 'Good',
@@ -28,38 +29,94 @@ describe('Review reducer testing', () => {
       }
     ]
   };
+  const reviews = [
+    {
+      user: '5f24ee06a2c4e08896cd1b3c',
+      _id: 1,
+      admin: '5f229ece17d7768ccff85190',
+      text: 'R&D',
+      bio: 'Good',
+      rating: 4,
+      reviewers: ['5f2253f77944be32c38936a1'],
+      feedbacks: [
+        {
+          _id: 1,
+          text: 'Good'
+        }
+      ]
+    },
+    {
+      user: '5f24ee06a2c4e08896cd1b3c',
+      _id: 2,
+      admin: '5f229ece17d7768ccff85190',
+      text: 'R&D',
+      bio: 'Good',
+      rating: 5,
+      reviewers: ['5f2253f77944be32c38936a1'],
+      feedbacks: [
+        {
+          _id: 1,
+          text: 'Good'
+        }
+      ]
+    }
+  ];
   test('returns default initial state when no action is passed', () => {
     const newState = reviewReducer(undefined, {});
     expect(newState).toEqual(defaultState);
   });
-  test('returns state of "false" on loading and not null on review upon receiving an action of type `GET_REVIEWS`', () => {
-    const newState = reviewReducer(undefined, { type: GET_REVIEWS });
+  test('returns state of "false" on loading and add reviews upon receiving an action of type `GET_REVIEWS`', () => {
+    const newState = reviewReducer(undefined, {
+      type: GET_REVIEWS,
+      payload: reviews
+    });
     expect(newState.loading).toBe(false);
-    expect(newState.reviews).not.toBe(null);
+    expect(newState.reviews).toEqual(reviews);
   });
-  test('returns state of "false" on loading and not null on error upon receiving an action of type `REVIEW_ERROR`', () => {
-    const newState = reviewReducer(undefined, { type: REVIEW_ERROR });
+  test('returns state of "false" on loading and add error upon receiving an action of type `REVIEW_ERROR`', () => {
+    const payload = { msg: 'Profile updated', alertType: 'success', id: 1 };
+    const newState = reviewReducer(undefined, { type: REVIEW_ERROR, payload });
     expect(newState.loading).toBe(false);
-    expect(newState.error).not.toBe(null);
+    expect(newState.error).toEqual(payload);
   });
-  test('returns state of "false" on loading upon receiving an action of type `DELETE_REVIEW`', () => {
-    const newState = reviewReducer(undefined, { type: DELETE_REVIEW });
-    expect(newState.loading).toBe(false);
-  });
-
-  test('returns state of "false" on loading upon receiving an action of type `ADD_REVIEW`', () => {
+  test('returns state of "false" on loading and delete one review from reviews upon receiving an action of type `DELETE_REVIEW`', () => {
     const newState = reviewReducer(
-      { ...defaultState, review },
-      { type: ADD_REVIEW }
+      { ...defaultState, reviews },
+      { type: DELETE_REVIEW, payload: 1 }
     );
     expect(newState.loading).toBe(false);
-    expect(newState.reviews).not.toBe(null);
+    expect(newState.reviews.length).toBe(1);
   });
 
-  test('returns state of "false" on loading and not null on review upon receiving an action of type `GET_REVIEW`', () => {
-    const newState = reviewReducer(undefined, { type: GET_REVIEW });
+  test('returns state of "false" on loading and review to reviews upon receiving an action of type `ADD_REVIEW`', () => {
+    const newState = reviewReducer(undefined, {
+      type: ADD_REVIEW,
+      payload: review
+    });
     expect(newState.loading).toBe(false);
-    expect(newState.review).not.toBe(null);
+    expect(newState.reviews.length).toBe(1);
+  });
+
+  test('returns state of "false" on loading and update review by id upon receiving an action of type `UPDATE_REVIEW`', () => {
+    const payload = { data: review, id: 1 };
+    const newState = reviewReducer(
+      { ...defaultState, reviews },
+      {
+        type: UPDATE_REVIEW,
+        payload
+      }
+    );
+    expect(newState.loading).toBe(false);
+    expect(newState.reviews.find(review => review._id === 1)).toEqual(review);
+  });
+
+  test('returns state of "false" on loading and add one review to review upon receiving an action of type `GET_REVIEW`', () => {
+    const newState = reviewReducer(undefined, {
+      type: GET_REVIEW,
+      payload: review
+    });
+    expect(newState.loading).toBe(false);
+    expect(newState.review).toEqual(review);
   });
 
   test('returns state of null on review upon receiving an action of type `CLEAR_REVIEW`', () => {
@@ -69,19 +126,36 @@ describe('Review reducer testing', () => {
     );
     expect(newState.review).toBeNull();
   });
-  test('returns state of "false" on loading and not null on review upon receiving an action of type `ADD_FEEDBACK`', () => {
+  test('returns state of "false" on loading and add feedback to review upon receiving an action of type `ADD_FEEDBACK`', () => {
+    const payload = { text: 'Good', _id: 1, name: 'Amy' };
     const newState = reviewReducer(
       { ...defaultState, review },
-      { type: ADD_FEEDBACK }
+      { type: ADD_FEEDBACK, payload }
     );
     expect(newState.loading).toBe(false);
-    expect(newState.review).not.toBe(null);
+    expect(newState.review.feedbacks).toEqual(payload);
   });
-  test('returns state of "false" on loading upon receiving an action of type `REMOVE_FEEDBACK`', () => {
+  test('returns state of "false" on loading and remove feedback by id from review upon receiving an action of type `REMOVE_FEEDBACK`', () => {
     const newState = reviewReducer(
-      { ...defaultState, review },
-      { type: REMOVE_FEEDBACK }
+      {
+        ...defaultState,
+        review: {
+          ...review,
+          feedbacks: [
+            { text: 'Good', _id: 1, name: 'Amy' },
+            { text: 'Good', _id: 2, name: 'Andy' }
+          ]
+        }
+      },
+      { type: REMOVE_FEEDBACK, payload: 1 }
     );
     expect(newState.loading).toBe(false);
+    expect(newState.review.feedbacks).toEqual([
+      {
+        text: 'Good',
+        _id: 2,
+        name: 'Andy'
+      }
+    ]);
   });
 });
