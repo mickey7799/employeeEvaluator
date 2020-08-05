066ac5bd27684d7a0f15e9b898fa93266469d698
Mickey <vickeylee.w@gmail.com>
2020-08-05 17:58:23 +1000

    Update profile reducer testing

--- client/src/reducers/profile.test.js
+++ client/src/reducers/profile.test.js
@@ -59,23 +59,42 @@ describe('Profile reducer testing', () => {
     const newState = profileReducer(undefined, {});
     expect(newState).toEqual(defaultState);
   });
-  test('returns state of "false" on loading and not null on profile upon receiving an action of type `GET_PROFILE`', () => {
-    const newState = profileReducer(undefined, { type: GET_PROFILE });
-    expect(newState.profile).not.toBe(null);
+  test('returns state of "false" on loading and add profile upon receiving an action of type `GET_PROFILE`', () => {
+    const payload = {
+      skills: ['JS'],
+      user: { name: 'Amy' },
+      department: 'R&D',
+      bio: '',
+      status: 'Intern'
+    };
+    const newState = profileReducer(undefined, { type: GET_PROFILE, payload });
+    expect(newState.profile).toEqual(payload);
     expect(newState.loading).toBe(false);
   });
-  test('returns state of "false" on loading and not null on profile upon receiving an action of type `UPDATE_PROFILE`', () => {
-    const newState = profileReducer(undefined, { type: UPDATE_PROFILE });
-    expect(newState.profile).not.toBe(null);
+  test('returns state of "false" on loading and add profile upon receiving an action of type `UPDATE_PROFILE`', () => {
+    const payload = {
+      skills: ['JS'],
+      user: { name: 'Amy' },
+      department: 'R&D',
+      bio: '',
+      status: 'Intern'
+    };
+    const newState = profileReducer(undefined, {
+      type: UPDATE_PROFILE,
+      payload
+    });
+    expect(newState.profile).toEqual(payload);
     expect(newState.loading).toBe(false);
   });
-  test('returns state of "false" on loading and null on profile upon receiving an action of type `PROFILE_ERROR`', () => {
+  test('returns state of "false" on loading, add error, and null on profile upon receiving an action of type `PROFILE_ERROR`', () => {
+    const payload = { msg: 'Profile updated', alertType: 'success', id: 1 };
     const newState = profileReducer(
       { ...defaultState, profiles },
-      { type: PROFILE_ERROR }
+      { type: PROFILE_ERROR, payload }
     );
     expect(newState.loading).toBe(false);
     expect(newState.profile).toBeNull();
+    expect(newState.error).toEqual(payload);
   });
 
   test('returns state of "false" on loading, not null on repos, and null on profile upon receiving an action of type `CLEAR_PROFILE`', () => {
@@ -88,15 +107,21 @@ describe('Profile reducer testing', () => {
     expect(newState.profile).toBeNull();
   });
 
-  test('returns state of "false" on loading and not null on profiles upon receiving an action of type `GET_PROFILES`', () => {
-    const newState = profileReducer(undefined, { type: GET_PROFILES });
+  test('returns state of "false" on loading and add profiles upon receiving an action of type `GET_PROFILES`', () => {
+    const newState = profileReducer(undefined, {
+      type: GET_PROFILES,
+      payload: profiles
+    });
     expect(newState.loading).toBe(false);
-    expect(newState.profiles).not.toBe(null);
+    expect(newState.profiles).toEqual(profiles);
   });
 
-  test('returns state of "false" on loading and not null on repos upon receiving an action of type `GET_REPOS`', () => {
-    const newState = profileReducer(undefined, { type: GET_REPOS });
-    expect(newState.repos).not.toBe(null);
+  test('returns state of "false" on loading and add repos upon receiving an action of type `GET_REPOS`', () => {
+    const newState = profileReducer(undefined, {
+      type: GET_REPOS,
+      payload: repos
+    });
+    expect(newState.repos).toEqual(repos);
     expect(newState.loading).toBe(false);
   });
 });
